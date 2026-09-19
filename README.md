655eea4492cf2d5a5ce9aab877c321f7fadee399
Arslan Ahmad <47880584+de-ar@users.noreply.github.com>
2026-09-19 06:11:10 +0500

    refactor: what's in the name

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A javascript based simple calculator
 
 # What did I do
 Just took an image from Google and crop it's basics buttons, added css to make those piece look like the original image. Removed the screen then added two input fields with background having exactly like a real calculator's screen. Following is the image I took from the internet. I took this image back in Jan, 2017. Now, i can't find an exact reference/url to this image.  
-![Calculator](assets/images/poi.jpg) 
+![Calculator](assets/images/calculator-reference.jpg)
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,45 @@
-function func1(p) {
+function appendDigit(p) {
     var a = document.getElementById("screen").value;
     a = a + p;
     document.getElementById("screen").value = a;
 }
 
-function divsign() {
+function pressDivide() {
     var a = document.getElementById("screen").value;
     a = a + "/";
     document.getElementById("screen").value = a;
 }
 
-function mulsign() {
+function pressMultiply() {
     var a = document.getElementById("screen").value;
     a = a + "*";
     document.getElementById("screen").value = a;
 }
 
-function addsign() {
+function pressAdd() {
     var a = document.getElementById("screen").value;
     a = a + "+";
     document.getElementById("screen").value = a;
 }
 
-function subsign() {
+function pressSubtract() {
     var a = document.getElementById("screen").value;
     a = a + "-";
     document.getElementById("screen").value = a;
 }
 
-function pntsign() {
+function pressPoint() {
     var a = document.getElementById("screen").value;
     a = a + ".";
     document.getElementById("screen").value = a;
 }
 
-function func2() {
+function evaluate() {
     var a = document.getElementById("screen").value;
     document.getElementById("result").value = eval(a);
 }
 
-function func3() {
+function clearScreen() {
     var a = document.getElementById("screen").value;
     document.getElementById("screen").value = null;
 }
